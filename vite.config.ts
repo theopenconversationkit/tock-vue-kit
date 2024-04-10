e9c76868ed2d8fb6a9037780b8c0b4572df9e9ec
rkuffer <rodolphe.kuffer@partnre.com>
2024-04-10 15:21:43 +0200

    Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,45 @@
 /// <reference types="vite/client" />
 
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-// import { resolve } from 'path';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  console.log('The mode : ', mode);
+  console.log("The mode : ", mode);
   return {
-    plugins: [vue()],
+    plugins: [vue(), dts()],
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(mode),
     },
     build: {
-      // lib: {
-      //   // Could also be a dictionary or array of multiple entry points
-      //   entry: resolve(__dirname, 'src/main.ts'),
-      //   name: 'TockVue',
-      //   // the proper extensions will be added
-      //   fileName: 'index'
-      // },
-      rollupOptions: {
-        output: {
-          format: 'es',
-          entryFileNames: `assets/[name].js`,
-          chunkFileNames: `assets/[name].js`,
-          assetFileNames: `assets/[name].[ext]`
-        },
-        treeshake: true
-        // preserveEntrySignatures: 'strict'
+      lib: {
+        // Could also be a dictionary or array of multiple entry points
+        entry: resolve(__dirname, "src/index.ts"),
+        name: "TockVueKit",
+        // the proper extensions will be added
+        fileName: "tock-vue-kit",
+        // formats: ["es"],
       },
-      minify: mode === 'production'
-    }
+      // rollupOptions: {
+      //   output: {
+      //     // format: "es",
+      //     // entryFileNames: `assets/[name].js`,
+      //     // chunkFileNames: `assets/[name].js`,
+      //     // assetFileNames: `assets/[name].[ext]`,
+      //   },
+      //   treeshake: true,
+      //   // preserveEntrySignatures: 'strict'
+      // },
+      minify: mode === "production",
+    },
   };
 });
