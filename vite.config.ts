0cd247c10ed334040239924d72d325d5d08502c8
rkuffer <rodolphe.kuffer@partnre.com>
2024-07-04 15:03:07 +0200

    Demo update

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+export default defineConfig(({ command, mode }) => {
+  return {
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(mode),
+    },
+    base: mode === "production" ? "/tock-vue-kit/" : "/",
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
+});
